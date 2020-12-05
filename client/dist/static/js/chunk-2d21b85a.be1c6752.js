3b21b15ef5cba73934e868f5549ed6241e6c7205
junpengxu <weakeexu@gmai.com>
2020-12-05 15:08:52 +0800

    feat: add html

---
+++ client/dist/static/js/chunk-2d21b85a.be1c6752.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21b85a"],{bfaa:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticStyle:{padding:"30px"}},[t("router-view")],1)},u=[],c=t("2877"),i={},l=Object(c["a"])(i,a,u,!1,null,null,null);n["default"]=l.exports}}]);
