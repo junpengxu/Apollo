e5a85a83258d90c61ed9b03b083094a18f985201
junpengxu <weakeexu@gmai.com>
2021-01-31 11:59:36 +0800

    feat: update run server

--- server/manage.py
+++ server/manage.py
@@ -2,7 +2,7 @@
 # @Time    : 2020/10/17 12:59 上午
 # @Author  : xu.junpeng
 from app import create_app
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='127.0.0.1', debug=True, port=8000)
