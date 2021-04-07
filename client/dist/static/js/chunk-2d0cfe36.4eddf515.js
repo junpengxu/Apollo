b34f183a18f8e14944d72b64d9c160b4e6d360c8
junpengxu <weakeexu@gmai.com>
2021-04-08 00:52:49 +0800

    fix: fix url error

---
+++ client/dist/static/js/chunk-2d0cfe36.4eddf515.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cfe36"],{6644:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("待开发")])},a=[],u={name:"Index"},r=u,s=t("2877"),d=Object(s["a"])(r,c,a,!1,null,"28fd32a5",null);n["default"]=d.exports}}]);
--- client/src/api/operate-log.js
+++ client/src/api/operate-log.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-export function getAllServerNode(data) {
+export function searchOperateLog(data) {
   return request({
     url: '/operate_log/search',
     method: 'post',
